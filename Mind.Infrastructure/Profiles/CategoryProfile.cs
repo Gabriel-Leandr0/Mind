0e95a743b6a75a86a531ad7d79590d9830f54b76
Gabriel <gabriel.leandrods@gmail.com>
2024-03-13 23:47:18 -0300

    update project

---
+++ Mind.Infrastructure/Profiles/CategoryProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Mind.Domain.DTos;
+using Mind.Domain.Models;
+
+namespace Mind.Infrastructure.Profiles;
+
+public class CategoryProfile : Profile
+{
+    //Constructor
+    public CategoryProfile()
+    {
+        // Profile serve para mapear um objeto para outro
+        // - CreateMap<ClasseOrigem, ClasseDestino>()
+        // - ForMember(destino => destino.Propriedade, origem => origem.Propriedade)
+        CreateMap<Category, ReadCategoryDto>();
+        CreateMap<CreateCategoryDto, Category>();
+
+    }
+}
---
+++ Mind.Infrastructure/Profiles/ProjectProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Mind.Domain.DTos;
+using Mind.Domain.Models;
+
+namespace Mind.Infrastructure.Profiles;
+
+public class ProjectProfile : Profile
+{
+    //Constructor
+    public ProjectProfile()
+    {
+        // Profile serve para mapear um objeto para outro
+        // - CreateMap<ClasseOrigem, ClasseDestino>()
+        // - ForMember(destino => destino.Propriedade, origem => origem.Propriedade)
+        CreateMap<Project, ReadProjectDto>()
+            .ForMember(ReadProjectDto => ReadProjectDto.CategoryId,
+                opt => opt.MapFrom(Project => Project.CategoryId));
+        CreateMap<CreateProjectDto, Project>();
+
+    }
+}
